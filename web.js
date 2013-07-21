967776a223e75fd24bdc92d734f46b1ab7a17d1f
Aristide Onisan <aristidetraian@yahoo.com>
2013-07-21 09:56:06 +0200

    first commit

---
+++ web.js
@@ -0,0 +1,23 @@
+var express = require('express');
+
+var app = express.createServer(express.logger());
+
+var fs = require('fs');
+
+fs.readFile('index.html', 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+app.get('/', function(request, response) {
+  response.send(data);
+});
+
+
+});
+
+
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
